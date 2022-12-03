8d7e3e1bbfe467c7237ef259a4c67911038266f2
Michael Duong <michaelduong935@gmail.com>
2022-12-03 12:13:20 -0500

    Added a welcome message centered on the screen for the login page Fixed the pathing for the login, when login is completed successfully it redirects the user to the "index page"

--- cruciblefit/start/routes.py
+++ cruciblefit/start/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 
 start = Blueprint("start", __name__)
 
@@ -22,6 +22,12 @@ def view():
     return render_template("view.html")
 
 
-@start.route("/login")
+@start.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return render_template("index.html")
+    return render_template('login.html', error=error)
