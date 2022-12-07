40193f8e4adc417e9baed4deef17afb18dfd776f
Jason Garcia <jason.garcia.94@icloud.com>
2022-12-07 16:34:50 -0500

    We can now add foods, edit, and delete them.

--- cruciblefit/models.py
+++ cruciblefit/models.py
@@ -13,6 +13,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     workouts = db.relationship('Workout')
+    foods = db.relationship('Food') #added
 
 
 '''
@@ -40,11 +41,31 @@ class Exercise(db.Model):
 '''
 Nutrition
 '''
-
-
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
     protein = db.Column(db.Integer, nullable=False)
     carbs = db.Column(db.Integer, nullable=False)
     fats = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #added
+
+    @property
+    def calories(self):
+        protein_calc = self.protein * 4
+        carbs_calc = self.carbs * 4
+        fats_calc = self.fats * 9
+        total_calories = protein_calc + carbs_calc + fats_calc
+        return total_calories
+
+
+#Many-to-Many relationship
+#https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
+#Limitations: you can only add one food at a time. Can only add one food per day
+logs = db.Table('log_items',
+    db.Column('log_id', db.Integer, db.ForeignKey('log.id'),primary_key=True),
+    db.Column('food_id', db.Integer,  db.ForeignKey('food.id'),primary_key=True)
+)
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    date = db.Column(db.Date, nullable = False)
+    foods = db.relationship('Food', secondary=logs) 
--- cruciblefit/start/routes.py
+++ cruciblefit/start/routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, escape
+from flask import Blueprint, escape, redirect, url_for
 from flask import render_template, flash, request, session
 from flask_sqlalchemy import session
 from flask_login import login_user, login_required, logout_user, current_user
-
+from cruciblefit.models import Food
+from cruciblefit.extensions import db
 start = Blueprint("start", __name__)
 
 '''This is a blueprint. Used to route the pages 
@@ -19,7 +20,52 @@ def index():
 @start.route("/add")
 @login_required
 def add():
-    return render_template("add.html", user=current_user)
+    foods = Food.query.all()
+    return render_template("add.html", user=current_user, foods=foods, food = None)
+
+@start.route("/add", methods = ['POST'])
+@login_required
+def add_food():
+    food_name = request.form.get('food-name')
+    proteins = request.form.get('protein')
+    carbs = request.form.get('carbohydrates')
+    fats = request.form.get('fat')
+
+    food_id = request.form.get('food-id')
+
+    #being able to edit and update values
+    if(food_id):
+        food = Food.query.get(food_id)
+        food.name = food_name
+        food.protein = proteins
+        food.carbs = carbs
+        food.fats = fats
+    
+    else:
+        #creates new food if id does not exist
+        new_food = Food(name=food_name, protein = proteins, carbs = carbs,
+        fats = fats)
+
+        db.session.add(new_food) #adds new food to db
+
+    db.session.commit() 
+    return redirect(url_for('start.add'))
+
+@start.route('/delete_food/<int:food_id>')
+@login_required
+def delete_food(food_id):
+    food = Food.query.get(food_id)
+    db.session.delete(food)
+    db.session.commit()
+
+    return redirect(url_for('start.add'))
+
+@start.route('/edit_food/<int:food_id>')
+@login_required
+def edit_food(food_id):
+    food = Food.query.get(food_id)
+    foods = Food.query.all()
+    return render_template('add.html', food = food, foods = foods)
 
 
 @start.route("/view")
